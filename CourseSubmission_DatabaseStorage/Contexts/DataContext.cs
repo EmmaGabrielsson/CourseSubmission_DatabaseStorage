7210423281f3e653e37c8618d216bf5c3cf51543
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2023-03-09 18:55:47 +0100

    Add project files.

---
+++ CourseSubmission_DatabaseStorage/Contexts/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseSubmission_DatabaseStorage.Contexts;
+
+internal class DataContext : DbContext
+{
+    #region Constructors and Overrides
+    public DataContext()
+    {
+    }
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\emmag\source\repos\Datalagring\CourseSubmission_DatabaseStorage\CourseSubmission_DatabaseStorage\Contexts\SqlDb.mdf;Integrated Security=True;Connect Timeout=30");
+    }
+    #endregion
+
+
+}
